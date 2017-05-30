b320e2efe4a0a47190a114d43b5b80140bf41d0e
Ondřej Šírek <siro.kunden@gmail.com>
2017-05-30 20:02:52 +0200

    New comics in CZ

--- _site/js/new-age.js
+++ _site/js/new-age.js
@@ -28,29 +28,62 @@
         }
     });
     
+    function updateRealese() {
+        var releaseDate = new Date();
+        releaseDate.setDate(releaseDate.getDate() + (1 + 7 - releaseDate.getDay()) % 7);
+        releaseDate.setHours(20);
+        releaseDate.setMinutes(0);
+        releaseDate.setSeconds(0);
+        var diff = Math.abs(releaseDate - new Date());
+        diff = diff / 1000;
+        var days = Math.floor(diff / 86400);
+        diff = diff - (days * 86400);
+        var hours = Math.floor(diff / 3600);
+        diff = diff - (hours * 3600);
+        var minutes = Math.floor(diff / 60);
+        diff = diff - (minutes * 60);
+        var seconds = Math.floor(diff);
+
+        $("#releaseDate").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+    }
+    
     $( document ).ready(function() {
+        
+        for(var i = 0; i < comicsDownloaded.length; i++) {
+            $("#comicsBody").append('<tr><td><img src="https://comicsdb.cz' + comicsDownloaded[i].cover + '" height="100px" /></td><td><a href="https://comicsdb.cz/' + comicsDownloaded[i].url + '" target="_blank">' + comicsDownloaded[i].name + '</a></td><td>' + comicsDownloaded[i].price + '</td><td>' + comicsDownloaded[i].publisher + '</td><td>' + comicsDownloaded[i].publish + '</td><td>' + comicsDownloaded[i].type + '</td><td>' + comicsDownloaded[i].format + '</td><td>' + comicsDownloaded[i].pages + '</td></tr>');
+        }
+        
+        var t = $('#comics').DataTable({
+            responsive: true,
+            "order": [[ 4, "desc" ]],
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Czech.json"
+            },
+            'sDom': 't<"bottom"p>'
+        });
         
         setInterval(updateRealese, 1000);
 
-        function updateRealese() {
-            var releaseDate = new Date();
-            releaseDate.setDate(releaseDate.getDate() + (1 + 7 - releaseDate.getDay()) % 7);
-            releaseDate.setHours(20);
-            releaseDate.setMinutes(0);
-            releaseDate.setSeconds(0);
-            var diff = Math.abs(releaseDate - new Date());
-            diff = diff / 1000;
-            var days = Math.floor(diff / 86400);
-            diff = diff - (days * 86400);
-            var hours = Math.floor(diff / 3600);
-            diff = diff - (hours * 3600);
-            var minutes = Math.floor(diff / 60);
-            diff = diff - (minutes * 60);
-            var seconds = Math.floor(diff);
-           
-            $("#releaseDate").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
-        }
-        
     });
 
 })(jQuery); // End of use strict
+
+function downloadSourceOfHtml(url, url_part, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(xhttp.responseText, url_part);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function search(urlKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].url === urlKey) {
+            return myArray[i];
+        }
+    }
+    return null;
+}
